46f67bbe854f5f331506dda1a278e4d97abec58d
KZS <KZS@a.a>
2023-11-27 22:03:18 +0100

    számok középen

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-const gameArea = document.querySelector('#gamearea')
-const timerDisplay = document.querySelector('#timer')
-let startTime
-
-// Az 1-től 12-ig tartó véletlen számok generálása
-const numbers = Array.from({ length: 12 }, (_, i) => i + 1)
-numbers.sort(() => Math.random() - 0.5)
-
-numbers.forEach((number) => {
-  const szamDoboz = document.createElement('div')
-  szamDoboz.classList.add('szamDoboz')
-  szamDoboz.textContent = number
-
-  szamDoboz.addEventListener('click', function () {
-    if (!startTime) {
-      startTime = Date.now()
-      startTimer()
-    }
-    this.style.visibility = 'hidden'
-  })
-
-  gameArea.appendChild(szamDoboz)
-})
-
-function startTimer() {
-  setInterval(() => {
-    if (startTime) {
-      const currentTime = Date.now()
-      const elapsedTime = currentTime - startTime
-      timerDisplay.textContent = 'Eltelt idő: ' + formatTime(elapsedTime)
-    }
-  }, 1000)
-}
-
-function formatTime(milliseconds) {
-  const totalSeconds = Math.floor(milliseconds / 1000)
-  const minutes = Math.floor(totalSeconds / 60)
-  const seconds = totalSeconds % 60
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+const gameArea = document.querySelector("#gamearea");
+for (let i = 0; i < 12; i++) {
+  const szamDoboz = document.createElement("div");
+  szamDoboz.innerHTML = i + 1;
+  gameArea.appendChild(szamDoboz);
+  szamDoboz.addEventListener("click", () => {
+    szamDoboz.style.visibility = "hidden";
+  });
 }
