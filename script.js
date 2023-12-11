6f6d13fbeb2f0dfe8e6200a3cf3cdeac6ecf0ce0
KZS <KZS@a.a>
2023-12-11 19:28:57 +0100

    proba

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const gameArea = document.querySelector("#gamearea");
+
+let t = [];
+
+for (let i = 0; i < 100; i++) {
+  let pos1 = Math.floor(Math.random() * 12);
+  let pos2 = Math.floor(Math.random() * 12);
+  t[i] = [pos1];
+  pos1[i] = [pos2];
+  pos2[i] = t;
+}
+
 for (let i = 0; i < 12; i++) {
   const szamDoboz = document.createElement("div");
-  szamDoboz.innerHTML = i + 1;
+  szamDoboz.innerHTML = t[i];
   gameArea.appendChild(szamDoboz);
   szamDoboz.addEventListener("click", () => {
     szamDoboz.style.visibility = "hidden";
