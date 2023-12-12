b84bd984e236629d98e0ff88db8d2027bceb6274
KolozsLaszlo <119128615+KolozsLaszlo@users.noreply.github.com>
2023-12-12 06:53:08 +0100

    hf done

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const gameArea = document.querySelector("#gamearea");
 
-let t = [];
 
-for (let i = 0; i < 100; i++) {
-  let pos1 = Math.floor(Math.random() * 12);
-  let pos2 = Math.floor(Math.random() * 12);
-  t[i] = [pos1];
-  pos1[i] = [pos2];
-  pos2[i] = t;
+let t=[]
+for(let i=0;i<12;i++)
+{
+  t.push(i+1);
+}
+
+for(let i=0;i<100;i++)
+{
+  let pos1=Math.floor(Math.random()*12);
+  let pos2=Math.floor(Math.random()*12);
+  let temp=t[pos1];
+  t[pos1]=t[pos2];
+  t[pos2]=temp;
+
 }
 
 for (let i = 0; i < 12; i++) {
