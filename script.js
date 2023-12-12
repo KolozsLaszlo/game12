057a8577612cebc9d8cc7342d8a411412134978c
Kolozs <Kolozs@a.a>
2023-12-12 10:15:48 +0100

    időzítő részben megcsinálva

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-const gameArea = document.querySelector("#gamearea");
+const gameArea = document.querySelector('#gamearea')
 
 //#region ------- tömb feltöltés
-let t = [];
+let t = []
 for (let i = 0; i < 12; i++) {
-  t.push(i + 1);
+  t.push(i + 1)
 }
 //#endregion
 
 //#region -------- keverés
 for (let i = 0; i < 100; i++) {
-  let pos1 = Math.floor(Math.random() * 12);
-  let pos2 = Math.floor(Math.random() * 12);
-  let temp = t[pos1];
-  t[pos1] = t[pos2];
-  t[pos2] = temp;
+  let pos1 = Math.floor(Math.random() * 12)
+  let pos2 = Math.floor(Math.random() * 12)
+  let temp = t[pos1]
+  t[pos1] = t[pos2]
+  t[pos2] = temp
 }
 //#endregion
-
+let NextNumber = 1
 //#region  ------- Számok megjelenítése
 for (let i = 0; i < 12; i++) {
-  const szamDoboz = document.createElement("div");
-  szamDoboz.innerHTML = t[i];
-  gameArea.appendChild(szamDoboz);
-  szamDoboz.addEventListener("click", () => {
-    szamDoboz.style.visibility = "hidden";
-  });
+  const szamDoboz = document.createElement('div')
+  szamDoboz.innerHTML = t[i]
+  gameArea.appendChild(szamDoboz)
+
+  szamDoboz.addEventListener('click', () => {
+    szamDoboz.classList.add('rejtett')
+    NextNumber++
+    if (NextNumber == 13) {
+      clearInterval(idozito)
+    }
+  })
 }
 //#endregion
 
 //#region ------- stopper
-const szamlalo = document.querySelector("#szamlalo");
-let ido = 0;
-setInterval(function () {
-  szamlalo.innerHTML = ido;
-  ido++;
-}, 1000);
+const szamlalo = document.querySelector('#szamlalo')
+let ido = 0
+const idozito = setInterval(function () {
+  szamlalo.innerHTML = ido / 10
+  ido++
+}, 100)
 //#endregion
