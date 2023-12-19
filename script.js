278221a345d355aa24950ae0676bd67b2c670c69
KZS <KZS@a.a>
2023-12-19 08:24:22 +0100

    oraimunka12_19

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-const gameArea = document.querySelector('#gamearea')
+const gameArea = document.querySelector("#gamearea");
 
 //#region ------- tömb feltöltés
-let t = []
+let t = [];
 for (let i = 0; i < 12; i++) {
-  t.push(i + 1)
+  t.push(i + 1);
 }
 //#endregion
 
 //#region -------- keverés
 for (let i = 0; i < 100; i++) {
-  let pos1 = Math.floor(Math.random() * 12)
-  let pos2 = Math.floor(Math.random() * 12)
-  let temp = t[pos1]
-  t[pos1] = t[pos2]
-  t[pos2] = temp
+  let pos1 = Math.floor(Math.random() * 12);
+  let pos2 = Math.floor(Math.random() * 12);
+  let temp = t[pos1];
+  t[pos1] = t[pos2];
+  t[pos2] = temp;
 }
 //#endregion
-let NextNumber = 1
+let NextNumber = 1;
 //#region  ------- Számok megjelenítése
 for (let i = 0; i < 12; i++) {
-  const szamDoboz = document.createElement('div')
-  szamDoboz.innerHTML = t[i]
-  gameArea.appendChild(szamDoboz)
+  const szamDoboz = document.createElement("div");
+  szamDoboz.innerHTML = t[i];
+  gameArea.appendChild(szamDoboz);
 
-  szamDoboz.addEventListener('click', () => {
-    szamDoboz.classList.add('rejtett')
-    NextNumber++
+  szamDoboz.addEventListener("click", () => {
+    szamDoboz.classList.add("rejtett");
+    NextNumber++;
     if (NextNumber == 13) {
-      clearInterval(idozito)
+      clearInterval(idozito);
     }
-  })
+  });
 }
 //#endregion
 
 //#region ------- stopper
-const szamlalo = document.querySelector('#szamlalo')
-let ido = 0
-const idozito = setInterval(function () {
-  szamlalo.innerHTML = ido / 10
-  ido++
-}, 100)
+
+const start = document.querySelector("#start");
+const szamlalo = document.querySelector("#szamlalo");
+let ido = 0;
+start.addEventListener("click", () => {
+  const idozito = setInterval(function () {
+    szamlalo.innerHTML = ido / 10;
+    ido++;
+  }, 100);
+});
 //#endregion
