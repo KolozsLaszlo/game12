bad6eef62ed9333f0c204a1d673e404c76b5f441
KZS <KZS@a.a>
2024-01-08 17:26:26 +0100

    style added

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-const gameArea = document.querySelector('#gamearea')
-const start = document.querySelector('#start')
-let t = []
-const szamlalo = document.querySelector('#szamlalo')
-let ido = 0
-let idozito
-let NextNumber
-let idok = []
+const gameArea = document.querySelector("#gamearea");
+const start = document.querySelector("#start");
+let t = [];
+const szamlalo = document.querySelector("#szamlalo");
+let ido = 0;
+let idozito;
+let NextNumber;
+let idok = [];
 
 function initNumbers() {
   for (let i = 0; i < 12; i++) {
-    t.push(i + 1)
+    t.push(i + 1);
   }
 }
 
 function shuffleNumbers() {
   for (let i = 0; i < 100; i++) {
-    let pos1 = Math.floor(Math.random() * 12)
-    let pos2 = Math.floor(Math.random() * 12)
-    let temp = t[pos1]
-    t[pos1] = t[pos2]
-    t[pos2] = temp
+    let pos1 = Math.floor(Math.random() * 12);
+    let pos2 = Math.floor(Math.random() * 12);
+    let temp = t[pos1];
+    t[pos1] = t[pos2];
+    t[pos2] = temp;
   }
 }
 
 function createBoxes() {
   for (let i = 0; i < 12; i++) {
-    const szamDoboz = document.createElement('div')
+    const szamDoboz = document.createElement("div");
     // szamDoboz.innerHTML = t[i]
-    szamDoboz.classList.add('rejtett')
-    gameArea.appendChild(szamDoboz)
+    szamDoboz.classList.add("rejtett");
+    gameArea.appendChild(szamDoboz);
 
-    szamDoboz.addEventListener('click', () => {
-      szamDoboz.classList.add('rejtett')
-      NextNumber++
+    szamDoboz.addEventListener("click", () => {
+      szamDoboz.classList.add("rejtett");
+      NextNumber++;
       if (NextNumber == 13) {
-        clearInterval(idozito)
+        clearInterval(idozito);
       }
-    })
+    });
   }
 }
 
 function fillBoxes() {
-  const szamdobozok = gameArea.querySelectorAll('div')
-  let i = 0
+  const szamdobozok = gameArea.querySelectorAll("div");
+  let i = 0;
   for (szamDoboz of szamdobozok) {
-    szamDoboz.innerText = t[i]
-    szamDoboz.classList.remove('rejtett')
-    i++
+    szamDoboz.innerText = t[i];
+    szamDoboz.classList.remove("rejtett");
+    i++;
   }
 }
 
 function startTimer() {
   idozito = setInterval(function () {
-    szamlalo.innerHTML = ido / 10
-    ido++
-  }, 100)
+    szamlalo.innerHTML = ido / 10;
+    ido++;
+  }, 100);
 }
 
-initNumbers()
-createBoxes()
+initNumbers();
+createBoxes();
 
-start.addEventListener('click', function () {
-  NextNumber = 0
-  clearInterval(idozito)
-  ido = 0
-  shuffleNumbers()
-  fillBoxes()
-  startTimer()
-  NextNumber++
-  idok.push(ido)
-})
+start.addEventListener("click", function () {
+  NextNumber = 0;
+  clearInterval(idozito);
+  ido = 0;
+  shuffleNumbers();
+  fillBoxes();
+  startTimer();
+  NextNumber++;
+  idok.push(ido);
+});
